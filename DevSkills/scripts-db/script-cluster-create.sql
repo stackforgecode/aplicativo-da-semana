9005689c655ef460266167b684561f0cbe3aad38
Rubens Lyra <rubensvlyra@gmail.com>
2023-05-23 14:04:20 -0300

    chore: preparando o ambiente

--- DevSkills/scripts-db/script-cluster-create.sql
+++
@@ -1,4 +0,0 @@
--- Criar o cluster
-CREATE CLUSTERED INDEX IX_UsuarioDB ON UsuarioDB.Usuario (Id);
-CREATE CLUSTERED INDEX IX_DevSkillsDB ON DevSkillsDB.Developer (Id);
-CREATE CLUSTERED INDEX IX_AuditoriaDB ON AuditoriaDB.Auditoria (Id);
